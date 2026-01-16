6c67408f0aa664f22a733bfe65894808b62eda05
Dhracyr <moritz@goemmel.com>
2026-01-16 15:12:40 +0100

    :sparkles: Update CONST.py to cover copy-paste code as a comment

--- CONST.py
+++ CONST.py
@@ -8,3 +8,13 @@ def calculate_ann(x, Fs):
 def get_wav(Fs, name='01 Main Theme (Aberration).wav'):
     fn_wav = os.path.join('own_music_data', name)
     return librosa.load(fn_wav, sr=Fs)
+
+"""
+from CONST import get_wav, calculate_ann
+
+Fs = 22050
+x, Fs = get_wav(Fs, '01 Main Theme (Aberration).wav')
+ann, color_ann, x_duration = calculate_ann(x, Fs)
+
+fn_wav = os.path.join('own_music_data', '01 Main Theme (Aberration).wav')
+"""
